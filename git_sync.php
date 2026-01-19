4392e5abdc607867c6c953091d90b204c425fbef
vs-system <jahegdm@gmail.com>
2026-01-18 22:00:03 -0300

    depp

--- git_sync.php
+++ git_sync.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * GIT SYNC HELPER v2 - VS System ERP
  * Trying multiple execution methods.
@@ -17,7 +17,7 @@ function run_git($cmd)
 
     foreach ($methods as $method) {
         if (function_exists($method)) {
-            echo "<em>Intentando con método: $method</em><br>";
+            echo "<em>Intentando con mó©todo: $method</em><br>";
             try {
                 if ($method === 'shell_exec') {
                     $out = shell_exec($full_cmd);
@@ -41,19 +41,19 @@ function run_git($cmd)
                 }
 
                 if ($success) {
-                    echo "<span style='color:green;'>Comando ejecutado con éxito vía $method</span><br>";
+                    echo "<span style='color:green;'>Comando ejecutado con ó©xito vó­a $method</span><br>";
                     break;
                 }
             } catch (Error $e) {
                 echo "<span style='color:orange;'>Error con $method: " . $e->getMessage() . "</span><br>";
             }
         } else {
-            echo "<em>Método deshabilitado: $method</em><br>";
+            echo "<em>Mó©todo deshabilitado: $method</em><br>";
         }
     }
 
     if (!$success) {
-        echo "<b style='color:red;'>No se pudo ejecutar ningún comando. Todas las funciones de ejecución están deshabilitadas.</b><br>";
+        echo "<b style='color:red;'>No se pudo ejecutar ningóºn comando. Todas las funciones de ejecució³n estó¡n deshabilitadas.</b><br>";
     }
 }
 
@@ -61,11 +61,17 @@ echo "<h2>Git Recovery Tools</h2>";
 
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'fix') {
-        run_git('reset --hard HEAD');
-        run_git('pull origin main');
+        run_git('fetch --all');
+        run_git('reset --hard origin/main');
+        run_git('clean -fd'); // Esto borra archivos locales que no estén en Git para evitar conflictos
     }
 }
 
 echo "<ul>
-    <li><a href='?action=fix'>INTENTAR REPARACIÓN (Reset & Pull)</a></li>
+    <li><a href='?action=fix'>INTENTAR REPARACIó“N (Reset & Pull)</a></li>
 </ul>";
+
+
+
+
+
